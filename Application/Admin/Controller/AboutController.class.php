8c8b580f9452a2e8a0a617a82f0bbbbb11a21490
jinhan <569929309@qq.com>
2016-02-19 00:00:54 +0800

    2016/2/19企业介绍功能完成

--- Application/Admin/Controller/AboutController.class.php
+++ Application/Admin/Controller/AboutController.class.php
@@ -21,8 +21,26 @@ class AboutController extends CommomController{
 		$isexist=M('webabout')->where(array('username'=>$username))->find();
 		if($isexist===false){
 			$this->ajaxReturn('系统出错');
+			return;
 		}
-		
+		$data['about']=$about;
+		$data['username']=$username;
+		if($isexist==null){
+			$data['createtime']=date("Y-m-d H:i:s",strtotime('now'));
+			if(false===M('webabout')->add($data)){
+				$this->ajaxReturn('系统出错');
+				return;
+			}else{
+				$this->ajaxReturn('修改成功');
+				return;
+			}
+		}
+		$data['updatetime']=date("Y-m-d H:i:s",strtotime('now'));
+		if(false===M('webabout')->where(array('username'=>$username))->save($data)){
+			$this->ajaxReturn('系统出错');
+			return;
+		}
+		$this->ajaxReturn('修改成功');
 	}
 	
 	

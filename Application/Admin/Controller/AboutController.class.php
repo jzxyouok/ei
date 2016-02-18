0c21e804a9f6cd7abf87327bf2dc4f6dd3d988bd
jinhan <569929309@qq.com>
2016-02-18 23:32:42 +0800

    2016/2/18

--- Application/Admin/Controller/AboutController.class.php
+++ Application/Admin/Controller/AboutController.class.php
@@ -3,11 +3,26 @@ namespace Admin\Controller;
 use Think\Controller;
 class AboutController extends CommomController{
 	public function index(){
+		$username=$_SESSION['username'];
+		$isexist=M('webabout')->where(array('username'=>$username))->find();
+		if($isexist){
+			$this->assign('about',$isexist['about']);
+		}
 		$this->display();
 	}
 	
 	function help(){
 		$this->display();
+	}
+	
+	function updateabout(){
+		$username=$_SESSION['username'];
+		$about=I('about','');
+		$isexist=M('webabout')->where(array('username'=>$username))->find();
+		if($isexist===false){
+			$this->ajaxReturn('系统出错');
+		}
+		
 	}
 	
 	

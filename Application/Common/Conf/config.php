5586b29c3df6a7f472ed502769da494a7290ea42
jinhan <569929309@qq.com>
2016-02-20 23:48:01 +0800

    test

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -12,7 +12,7 @@ return array (
 		                                  
 		// 数据库表前缀URL访问模式,可选参数0、1、2、3,代表以下四种模式：
 		                                  // 0 (普通模式); 1 (PATHINFO 模式); 2 (REWRITE 模式); 3 (兼容模式) 默认为PATHINFO 模式
-		'URL_MODEL' => '1',
+		'URL_MODEL' => '2',
 		
 		'USER_AUTH_KEY' => 'userid', // 用户会话标识
 		'NEEDLOGIN' => 1, // 1表示登陆开启，其他表示关闭

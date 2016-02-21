fc9f7193e8a414ef7b857bcccb44c9366eefff8b
jinhan <569929309@qq.com>
2016-02-21 23:25:56 +0800

    2016/2/21更新企业介绍为单页模式

--- Application/Admin/Controller/AboutController.class.php
+++ Application/Admin/Controller/AboutController.class.php
@@ -2,17 +2,43 @@
 namespace Admin\Controller;
 use Think\Controller;
 class AboutController extends CommomController{
-	public function index(){
+	function index(){
+		$this->display();
+	}
+	public function indexs(){
 		$username=$_SESSION['username'];
-		$isexist=M('webabout')->where(array('username'=>$username))->find();
-		if($isexist){
-			$this->assign('about',$isexist['about']);
+		$status=I('status',1);
+		if(false==is_numeric($status)){
+			$this->display();
+			return;
 		}
+	//	$total=M('webabout')->where("username='".$username."' AND status!=3")->count();
+		$total=M('webabout')->where(array('username'=>$username,'status'=>$status))->count();
+		$page = I('page',1);
+		$pageStart = ($page - 1) * $pageSize;
+		$rows =M('webabout')->limit($pageStart.','.$pageSize)->where(array('username'=>$username,'status'=>$status))->order('createtime desc')->field('about,username',true)->select();
+		$data['total']=$total;
+		foreach ($rows as &$row){
+			if($row['status']==1){
+				$row['status']='已开启';
+			}elseif($row['status']==0){
+				$row['status']='已关闭';
+			}else{
+				$row['status']='回收站';
+			}
+		}
+		$data['rows']=$rows;
+		$this->ajaxReturn($data);
 		$this->display();
 	}
 	
 	function help(){
 		$this->display();
+	}
+	
+	//更新状态
+	function updatestatus(){
+		
 	}
 	
 	function updateabout(){

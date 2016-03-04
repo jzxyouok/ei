14caeadcd8b3ac5e615e758a8656a0fa07b1c715
hunterhug <gdccmcm14@live.com>
2016-03-04 17:50:24 +0800

    气死人

--- Application/Admin/Controller/QingmuController.class.php
+++ Application/Admin/Controller/QingmuController.class.php
@@ -23,19 +23,19 @@ class QingmuController extends CommomController {
 	
 	public function listcomments(){
 			$shop_name=I('shop_name','');
-			if($shop_name==''){
-			$total=M('qingmu.comment','c_')->count();
-			}else{
-			$total=M('qingmu.comment','c_')->where(array('shop_name'=>$shop_name))->count();
+			$url=I('url','');
+			//$a=array(1=>1);
+			if($shop_name!=''){
+			$a['shop_name']=$shop_name;
 			}
+			if($url!=''){
+			$a['item_url']=$url;
+			}
+			$total=M('qingmu.comment','c_')->where($a)->count();
 			$pageSize =I('rows','5');
 			$page = I('page','5');
 			$pageStart = ($page - 1) * $pageSize;
-			if($shop_name==''){
-			$rows =M('qingmu.comment','c_')->limit($pageStart.','.$pageSize)->order('createtime desc')->select();
-			}else{
-			$rows =M('qingmu.comment','c_')->where(array('shop_name'=>$shop_name))->limit($pageStart.','.$pageSize)->order('createtime desc')->select();
-			}
+			$rows =M('qingmu.comment','c_')->where($a)->limit($pageStart.','.$pageSize)->order('createtime desc')->select();
 			$data['total']=$total;
 			$data['rows']=$rows;
 			$this->ajaxReturn($data);
@@ -43,19 +43,21 @@ class QingmuController extends CommomController {
 	
 	public function listdictionarys(){
 			$type=I('type','');
-			if($type==''){
-			$total=M('qingmu.dictionary','c_')->count();
-			}else{
-			$total=M('qingmu.dictionary','c_')->where(array('type'=>$type))->count();
+			$nullscore=I('nullscore',1);
+			//$a=array(1=>1);
+			if($type!=''){
+			$a['type']=$type;
 			}
+			if($nullscore==0){
+			$a['score']=array('EXP','IS NULL');
+			}else{
+			$a['score']=array('EXP','IS NOT NULL');
+			}
+			$total=M('qingmu.dictionary','c_')->where($a)->count();
 			$pageSize =I('rows','5');
 			$page = I('page','5');
 			$pageStart = ($page - 1) * $pageSize;
-			if($type==''){
-			$rows =M('qingmu.dictionary','c_')->limit($pageStart.','.$pageSize)->order('score desc')->select();
-			}else{
-			$rows =M('qingmu.dictionary','c_')->where(array('type'=>$type))->limit($pageStart.','.$pageSize)->order('score desc')->select();
-			}
+			$rows =M('qingmu.dictionary','c_')->where($a)->limit($pageStart.','.$pageSize)->order('createtime desc')->select();
 			$data['total']=$total;
 			$data['rows']=$rows;
 			$this->ajaxReturn($data);
@@ -68,12 +70,15 @@ class QingmuController extends CommomController {
 	}
 	
 	function listrender(){
-	
-			$total=M('qingmu.result','c_')->count();
+			$url=I('url','');
+			if($url!=''){
+			$a['item_url']=$url;
+			}
+			$total=M('qingmu.result','c_')->where($a)->count();
 			$pageSize =I('rows','');
 			$page = I('page','');
 			$pageStart = ($page - 1) * $pageSize;
-			$rows =M('qingmu.result','c_')->limit($pageStart.','.$pageSize)->order('updatetime desc')->select();
+			$rows =M('qingmu.result','c_')->where($a)->limit($pageStart.','.$pageSize)->order('updatetime desc')->select();
 			foreach($rows as &$row){
 			$row['d1']=round($row['d1'],2);
 			$row['d2']=round($row['d2'],2);
@@ -88,7 +93,58 @@ class QingmuController extends CommomController {
 			$this->ajaxReturn($data);
 	}
 	
+	function updatescore(){
+		$id=I('id','');
+		$score=I('score','');
+		$username=$_SESSION ['username'];
+		$data['score']=$score;
+		$data['people']=$username;
+		$data['createtime']=date("Y-m-d H:i:s",strtotime('now'));
+		$isok=M('qingmu.dictionary','c_')->where(array('id'=>$id))->save($data);
+		if($isok){
+			$this->ajaxReturn('成功');
+		}else{
+		$this->ajaxReturn('出错');
+		}
+		
+		
 	
+	}
 	
+	function render1(){
+	$id=I('row','');
+	if($id==''){
+		$this->error("找不到雷达图");
+	}else{
+	$data=M('qingmu.result','c_')->where(array('item_url'=>$id))->find();
+	//$datas=M('qingmu.commentsb','c_')->where(array('item_url'=>$id))->select();
+	$this->assign('data',$data);
+	//$this->assign('datas',$datas);
+	$this->display();
+	}
+	}
+	
+	function listsb(){
+	$url=I('url','');
+			if($url!=''){
+			$a['item_url']=$url;
+			}
+			else{
+			return;
+			}
+			$total=M('qingmu.commentsb','c_')->where($a)->count();
+			$pageSize =I('rows','');
+			$page = I('page','');
+			$pageStart = ($page - 1) * $pageSize;
+			$rows =M('qingmu.commentsb','c_')->where($a)->limit($pageStart.','.$pageSize)->order('createtime desc')->select();
+			foreach($rows as &$row){
+			$commentid=$row['comment_id'];
+			$c=M('qingmu.comment','c_')->where(array('id'=>$commentid))->find();
+			$row['comment_id']=$c['content'];
+			}
+			$data['total']=$total;
+			$data['rows']=$rows;
+			$this->ajaxReturn($data);
+	}
 	
 }
